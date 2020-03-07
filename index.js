3bba7eb262c8d41e8ae4660d4bbed1b1946a88dc
NathanDennis <nathdennis@googlemail.com>
2020-03-07 05:04:20 -0500

    A little housekeeping before continuing logic and refactoring

--- index.js
+++ index.js
@@ -27,34 +27,43 @@ client.on('message', msg => {
     let intervalInteger = parseInt(checkForNumbersInMessage())
     let intervalVerb = ''
 
-    // Set current time and time to set reminder for
-    if (message === 'ping') {
-        msg.channel.send('pong')
-    }
-
+    // Assign values to intervalVerb & intervalInteger to determine reminderTime
     if (message.includes('minutes')) {
         intervalVerb = 'minutes'
     }
+
     if (message.includes('hours')) {
         intervalVerb = 'hours'
     }
+
+    if (message.includes('days')) {
+        intervalVerb = 'days'
+    }
+
+    if (message.includes('weeks')) {
+        intervalVerb = 'weeks'
+    }
+
+    if (message.includes('months')) {
+        intervalVerb = 'months'
+    }
     
+    // Logic for !remindme command
     if (message.startsWith('!remindme')) {
         const currentTime = moment().format('HH:mm')
         const reminderTime = moment().add(intervalInteger, intervalVerb).format('HH:mm')
         console.log(`Current time: ${currentTime}`)
         console.log(`Requested reminder time: ${reminderTime}`)
 
-        console.log(`Message from user: ${message}`)
-        console.log('==============')
-        console.log()
-        console.log(`Verb: ${intervalVerb}`)
-        console.log(typeof(intervalVerb))
-        console.log(`Integer: ${intervalInteger}`)
-        console.log(typeof(intervalInteger))
-        console.log('============')
-        console.log(`Current time: ${currentTime}`)
-        console.log(`Reminder time: ${reminderTime}`)
+        // console.log(`Message from user: ${message}`)
+        // console.log('==============')
+        // console.log(`Verb: ${intervalVerb}`)
+        // console.log(typeof(intervalVerb))
+        // console.log(`Integer: ${intervalInteger}`)
+        // console.log(typeof(intervalInteger))
+        // console.log('============')
+        // console.log(`Current time: ${currentTime}`)
+        // console.log(`Reminder time: ${reminderTime}`)
 
         const embed = new MessageEmbed()
             .setTitle('New reminder:')
@@ -62,15 +71,6 @@ client.on('message', msg => {
         msg.channel.send(embed)
     }
 })
-
-
-// client.on('message', message => {
-//     // If the message is "ping"
-//     if (message.content === 'ping') {
-//       // Send "pong" to the same channel
-//       message.channel.send('pong');
-//     }
-//   });
 
 client.login(TOKEN).then(() => {
     console.log('We logged in bois')
